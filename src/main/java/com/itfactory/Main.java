a741c921e07268be30aca8f5d6427f58f0e4d04a
AndreiR29 <andrei.raiu1998@gmail.com>
2023-07-04 21:42:49 +0300

    commit 3

--- src/main/java/com/itfactory/Main.java
+++ src/main/java/com/itfactory/Main.java
@@ -12,40 +12,42 @@ public class Main {
         System.out.println("3. Dreptunghi");
         System.out.print("Aleg optiunea: ");
 
-        int option = sc.nextInt();
-        double perimeter = 0;
+            int option = sc.nextInt();
+            double perimeter = 0;
 
-        switch (option) {
-            case 1:
-                System.out.println("Introduceti lungimea laturii 1: ");
-                double side1 = sc.nextDouble();
-                System.out.println("Introduceti lungimea laturii 2: ");
-                double side2 = sc.nextDouble();
-                System.out.println("Introduceti lungimea laturii 3: ");
-                double side3 = sc.nextDouble();
-                Triangle triangle = new Triangle(side1, side2, side3);
-                perimeter = triangle.calculatePerimeter();
-                System.out.println("Perimetrul triunghiului este egal cu " + perimeter);
-                break;
-            case 2:
-                System.out.println("Introduceti lungimea laturii patratului:");
-                double side = sc.nextDouble();
-                Square square = new Square(side);
-                perimeter = square.calculatePerimeter();
-                System.out.println("Perimetrul patratului este egal cu " + perimeter);
-                break;
-            case 3:
-                System.out.println("Introduceti lungimea dreptunghiului: ");
-                double length = sc.nextDouble();
-                System.out.println("Introduceti latimea dreptunghiului: ");
-                double width = sc.nextDouble();
-                Rectangle rectangle = new Rectangle(length, width);
-                perimeter = rectangle.calculatePerimeter();
-                System.out.println("Perimetrul triunghiului este egal cu " + perimeter);
-                break;
-            default:
-                throw new IllegalArgumentException("Opțiune invalidă. Vă rugăm să alegeți una dintre opțiunile 1, 2 sau 3.");
-
-        }
+                switch (option) {
+                    case 1:
+                        System.out.print("Introduceti lungimea laturii 1: ");
+                        double side1 = sc.nextDouble();
+                        System.out.print("Introduceti lungimea laturii 2: ");
+                        double side2 = sc.nextDouble();
+                        System.out.print("Introduceti lungimea laturii 3: ");
+                        double side3 = sc.nextDouble();
+                        Triangle triangle = new Triangle(side1, side2, side3);
+                        perimeter = triangle.calculatePerimeter();
+                        System.out.println("Perimetrul triunghiului este egal cu " + perimeter);
+                        break;
+                    case 2:
+                        System.out.print("Introduceti lungimea laturii patratului: ");
+                        double side = sc.nextDouble();
+                        Square square = new Square(side);
+                        perimeter = square.calculatePerimeter();
+                        System.out.println("Perimetrul patratului este egal cu " + perimeter);
+                        break;
+                    case 3:
+                        System.out.print("Introduceti lungimea dreptunghiului: ");
+                        double length = sc.nextDouble();
+                        System.out.print("Introduceti latimea dreptunghiului: ");
+                        double width = sc.nextDouble();
+                        Rectangle rectangle = new Rectangle(length, width);
+                        perimeter = rectangle.calculatePerimeter();
+                        System.out.println("Perimetrul dreptunghiului este egal cu " + perimeter);
+                        break;
+                    default:
+                        throw new IllegalArgumentException("Opțiune invalidă. Vă rugăm să alegeți una dintre opțiunile 1, 2 sau 3.");
+                }
     }
 }
+
+
+
